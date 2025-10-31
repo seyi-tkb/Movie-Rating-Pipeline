2058b29faf3ca528568ed24b328167488b59877d
seyi-tkb <marvee006@gmail.com>
2025-10-31 12:17:38 +0100

    test: added fixtures in conftest. updated related files

---
+++ tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+import boto3
+from moto import mock_aws
+
+@pytest.fixture
+def movie_data():
+    """Simulated Bronze CSV movie dataset."""
+    return (
+        "item_id,movie_title,release_date,IMDb_URL,primary_genre\n"
+        "1,The Matrix,1999-03-31,https://www.imdb.com/title/tt0133093/,Sci-Fi\n"
+        "2,Titanic,1997-12-19,https://www.imdb.com/title/tt0120338/,Romance\n"
+        "3,Inception,2010-07-16,https://www.imdb.com/title/tt1375666/,Thriller\n"
+        "4,The Godfather,1972-03-24,https://www.imdb.com/title/tt0068646/,Crime\n"
+        "5,Toy Story,1995-11-22,https://www.imdb.com/title/tt0114709/,Animation\n"
+        "6,NaN,2018-02-16,https://www.imdb.com/title/tt1825683/,Action\n"
+    )
+
+@pytest.fixture
+@mock_aws
+def s3_mock_env():
+    """Creates a fake S3 environment with both Bronze and Silver buckets."""
+    s3 = boto3.client("s3", region_name="us-east-1")
+    s3.create_bucket(Bucket="movie-pipeline-silver")
+    s3.create_bucket(Bucket="movie-pipeline-bronze")
+    yield s3  # provides it to the test
--- tests/silver/test_integration.py
+++ tests/silver/test_integration.py
@@ -4,19 +4,7 @@ import boto3
 import pandas as pd
 from unittest.mock import patch
 from pipeline.b_silver.transform import prepare_movie_df
-from config import S3_BUCKET_SILVER, WATERMARKS_PATH
 
-
-
-movie_data = (
-    "item_id,movie_title,release_date,IMDb_URL,primary_genre\n"
-    "1,The Matrix,1999-03-31,https://www.imdb.com/title/tt0133093/,Sci-Fi\n"
-    "2,Titanic,1997-12-19,https://www.imdb.com/title/tt0120338/,Romance\n"
-    "3,Inception,2010-07-16,https://www.imdb.com/title/tt1375666/,Thriller\n"
-    "4,The Godfather,1972-03-24,https://www.imdb.com/title/tt0068646/,Crime\n"
-    "5,Toy Story,1995-11-22,https://www.imdb.com/title/tt0114709/,Animation\n"
-    "6,NaN,2018-02-16,https://www.imdb.com/title/tt1825683/,Action\n"       # has missing title
-)
 
 # test_movie_flow
 @pytest.mark.integration
@@ -47,7 +35,7 @@ def test_prepare_movie_df_integration(mock_init_client, movie_data):
          patch("config.WATERMARKS_PATH", "watermarks/watermarks.csv"):
 
         # Run
-        pipeline_start = pd.Timestamp("2011-01-01", tz="UTC")
+        pipeline_start = pd.Timestamp("2020-01-01", tz="UTC")
         kwargs = {"data_interval_start": pd.Timestamp("2020-12-31", tz="UTC")}
         prepare_movie_df(pipeline_start, **kwargs)
 
